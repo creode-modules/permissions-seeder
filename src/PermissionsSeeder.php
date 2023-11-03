4867a06cad0ad78df0613efecf3bf19e5fb7ea97
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-11-03 17:08:31 +0000

    Adds permissions seeder class.

--- src/PermissionsSeeder.php
+++ src/PermissionsSeeder.php
@@ -2,6 +2,87 @@
 
 namespace Creode\PermissionsSeeder;
 
-class PermissionsSeeder
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+abstract class PermissionsSeeder extends Seeder
 {
+    /**
+     * Determines if we should give super admin permissions to this group.
+     *
+     * @var boolean
+     */
+    protected $giveSuperAdminPermissions = true;
+
+    /**
+     * Determines if we should create a role for this group.
+     *
+     * @var boolean
+     */
+    protected $shouldCreateRole = true;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permissionKeys = $this->getPermissions();
+
+        $assetPermissions = [];
+        foreach ($permissionKeys as $permission) {
+            $permissionName = $permission . $this->getPermissionGroup();
+            $assetPermissions[] = Permission::create([
+                'group' => $this->getPermissionGroup(),
+                'name' => $permissionName
+            ]);
+        }
+
+        // Give super admin permissions.
+        if ($this->giveSuperAdminPermissions) {
+            $adminRole = Role::firstOrCreate(['name' => 'super-admin']);
+            $adminRole->givePermissionTo($assetPermissions);
+        }
+
+        // Create an Asset-Manager Role and assign some Permissions.
+        $assetRole = Role::create(['name' => $this->getRoleName()]);
+        $assetRole->givePermissionTo($assetPermissions);
+    }
+
+    /**
+     * Gets the permission keys to create.
+     *
+     * @return array
+     */
+    protected function getPermissions(): array
+    {
+        return [
+            'viewAny',
+            'view',
+            'update',
+            'create',
+            'delete',
+            'destroy',
+        ];
+    }
+
+    /**
+     * Gets the permission group to use.
+     *
+     * @return void
+     */
+    abstract protected function getPermissionGroup(): string;
+
+    /**
+     * Gets the role name to use.
+     *
+     * @return string
+     */
+    protected function getRoleName(): string
+    {
+        return strtolower($this->getPermissionGroup() . '-manager');
+    }
 }
